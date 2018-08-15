88e1c21f1243b2a54a3cd27e2d019c8107b7fb1f
bambuunet <ringo@bamubuu.net>
2018-08-15 14:42:10 +0900

    20180815

--- swe_go/SwissEph.go
+++ swe_go/SwissEph.go
@@ -1,5 +1,11 @@
 package SwissEph
 
-swe_calc() int {
+func swe_calc_ut(tjd_ut float, ipl int, iflag int, []xx float, serr string) int{
+  return swe_calc(tjd_ut + SweDate.getDeltaT(tjd_ut), ipl, iflag, xx, serr)
+}
+
+func swe_calc() int{
   
 }
+
+
